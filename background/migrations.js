642ab01d4b57b9e4ed542a1451147cae63636a73
Andrew Duthie <andrew@andrewduthie.com>
2020-01-02 18:46:43 -0500

    Revert migrations refactor to dynamic imports
    Firefox addons linter does not currently support
    
    See: https://github.com/mozilla/addons-linter/issues/2940
    
    Since migrations are currently limited and this was simply a refactoring, easier to revert and reconsider introducing later, than to fight with Firefox linter.
    
    Reverts b27e13557e843120868be12d76813ee14e6741ef

--- background/migrations.js
+++ background/migrations.js
@@ -1,58 +1,48 @@
 /**
- * Migration implementation.
+ * Object used for matching on update, including implementation of migration.
  *
- * @typedef {()=>void} SLUpdateMigration
+ * @typedef SLUpdateMigration
+ *
+ * @property {string}     fromVersion Version to match for migration.
+ * @property {() => void} migrate     Migration function.
  */
 
 /**
  * Implemented migrations.
  *
- * @type {string[]}
+ * @type {SLUpdateMigration[]}
  */
 const MIGRATIONS = [
-	'1.1.0',
+	{
+		fromVersion: '1.0.0',
+		migrate() {
+			let { initialState } = window.localStorage;
+			if ( ! initialState ) {
+				return;
+			}
+
+			try {
+				initialState = JSON.parse( initialState );
+				browser.storage.sync.set( { initialState } );
+				delete window.localStorage.initialState;
+			} catch ( error ) {}
+		},
+	},
 ];
-
-/**
- * Returns true if the candidate satisfies the given version, or false
- * otherwise. A candidate satisfies a version if it occurs after the version.
- *
- * @param {string} version   Version against which to compare.
- * @param {string} candidate Migration version.
- *
- * @return {boolean} Whether candidate satisfies the given version.
- */
-export function isVersionSatisfied( version, candidate ) {
-	const partsA = version.split( '.' );
-	const partsB = candidate.split( '.' );
-
-	for ( let i = 0; i < 3; i++ ) {
-		if ( Number( partsB[ i ] ) < Number( partsA[ i ] ) ) {
-			return false;
-		}
-	}
-
-	return version !== candidate;
-}
 
 /**
  * Assigns listener to handle `runtime.onInstalled` event, running migration
  * logic for version updates.
  */
 export function initialize() {
-	browser.runtime.onInstalled.addListener( async ( details ) => {
+	browser.runtime.onInstalled.addListener( ( details ) => {
 		const { reason, previousVersion } = details;
-		if ( reason !== 'update' || ! previousVersion ) {
+		if ( reason !== 'update' ) {
 			return;
 		}
 
-		for ( let i = 0; i < MIGRATIONS.length; i++ ) {
-			const version = MIGRATIONS[ i ];
-			if ( isVersionSatisfied( previousVersion, version ) ) {
-				/** @type {{default:SLUpdateMigration}} */
-				const { default: migrate } = await import( `./migrations/${ version }.js` );
-				migrate();
-			}
-		}
+		MIGRATIONS
+			.filter( ( { fromVersion } ) => fromVersion === previousVersion )
+			.forEach( ( { migrate } ) => migrate() );
 	} );
 }
