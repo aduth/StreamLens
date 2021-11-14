3d63df00190988bcf343db383b82ddf2e98c8931
Andrew Duthie <andrew@andrewduthie.com>
2021-11-14 14:46:55 -0500

    Flesh out Badge tests

--- background/components/badge.ts
+++ background/components/badge.ts
@@ -2,6 +2,21 @@ import { some, isEmpty, size } from 'lodash-es';
 import { useSelect } from '@streamlens/state';
 import { useEffect } from 'preact/hooks';
 
+/**
+ * Badge colors.
+ */
+export enum Colors {
+	/**
+	 * Badge color for error state.
+	 */
+	ERROR = '#bc152e',
+
+	/**
+	 * Badge colors for all other states.
+	 */
+	DEFAULT = '#4285F4',
+}
+
 function Badge() {
 	const auth = useSelect((state) => state.auth);
 	const streams = useSelect((state) => state.streams);
@@ -11,16 +26,16 @@ function Badge() {
 	useEffect(() => {
 		let text, color;
 
-		const hasTokenError = some(auth, { token: null }) || !isOnline;
-		if (hasTokenError) {
+		const isError = some(auth, { token: null }) || !isOnline;
+		if (isError) {
 			text = '?';
-			color = '#bc152e';
+			color = Colors.ERROR;
 		} else {
 			// Only compute live streams if at least one authorization exists, and
 			// each provider has received at least once.
 			const hasFetched = !isEmpty(auth) && size(auth) === size(lastReceived);
 			text = hasFetched ? String(data.length) : '';
-			color = '#4285f4';
+			color = Colors.DEFAULT;
 		}
 
 		browser.browserAction.setBadgeText({ text });
--- background/components/test/badge.tsx
+++ background/components/test/badge.tsx
@@ -5,8 +5,8 @@ import { useStubbedGlobal } from '@streamlens/stub-global';
 import { expect } from 'chai';
 import createStore from 'unistore';
 import { SyncStore } from 'unistore-browser-sync';
-import Badge from '../badge';
-import { SLState, DEFAULT_STATE } from '../../store';
+import Badge, { Colors } from '../badge';
+import { SLState, SLStream, DEFAULT_STATE } from '../../store';
 
 describe('Badge', () => {
 	useStubbedGlobal('browser.browserAction.setBadgeText');
@@ -14,11 +14,11 @@ describe('Badge', () => {
 
 	function renderWithState(state: Partial<SLState>) {
 		const store = createStore({ ...DEFAULT_STATE, ...state }) as SyncStore<SLState>;
-		return render(
-			<StoreContext.Provider value={store}>
-				<Badge />
-			</StoreContext.Provider>
-		);
+		return render(<Badge />, {
+			wrapper({ children }) {
+				return <StoreContext.Provider value={store} children={children} />;
+			},
+		});
 	}
 
 	context('offline', () => {
@@ -26,6 +26,89 @@ describe('Badge', () => {
 			renderWithState({ isOnline: false });
 
 			expect(browser.browserAction.setBadgeText).to.have.been.calledWith({ text: '?' });
+			expect(browser.browserAction.setBadgeBackgroundColor).to.have.been.calledWith({
+				color: Colors.ERROR,
+			});
+		});
+
+		it('sets error state after going offline', () => {
+			const { rerender } = renderWithState({ isOnline: true });
+			rerender({ isOnline: false });
+
+			expect(browser.browserAction.setBadgeText).to.have.been.calledWith({ text: '?' });
+			expect(browser.browserAction.setBadgeBackgroundColor).to.have.been.calledWith({
+				color: Colors.ERROR,
+			});
+		});
+
+		it('sets default state after coming offline', () => {
+			const { rerender } = renderWithState({ isOnline: false });
+			rerender({ isOnline: false });
+
+			expect(browser.browserAction.setBadgeText).to.have.been.calledWith({ text: '' });
+			expect(browser.browserAction.setBadgeBackgroundColor).to.have.been.calledWith({
+				color: Colors.DEFAULT,
+			});
+		});
+	});
+
+	context('error token', () => {
+		it('sets error state', () => {
+			renderWithState({ auth: { twitch: { token: null, user: { login: '' } } }, isOnline: true });
+
+			expect(browser.browserAction.setBadgeText).to.have.been.calledWith({ text: '?' });
+			expect(browser.browserAction.setBadgeBackgroundColor).to.have.been.calledWith({
+				color: Colors.ERROR,
+			});
+		});
+	});
+
+	context('without initial fetch', () => {
+		it('sets empty text', () => {
+			renderWithState({ auth: { twitch: { token: '', user: { login: '' } } }, isOnline: true });
+
+			expect(browser.browserAction.setBadgeText).to.have.been.calledWith({ text: '' });
+			expect(browser.browserAction.setBadgeBackgroundColor).to.have.been.calledWith({
+				color: Colors.DEFAULT,
+			});
+		});
+	});
+
+	context('with fetched data', () => {
+		it('sets stream count', () => {
+			renderWithState({
+				auth: { twitch: { token: '', user: { login: '' } } },
+				streams: {
+					data: [{} as SLStream, {} as SLStream],
+					lastReceived: {
+						twitch: Date.now(),
+					},
+				},
+				isOnline: true,
+			});
+
+			expect(browser.browserAction.setBadgeText).to.have.been.calledWith({ text: '2' });
+			expect(browser.browserAction.setBadgeBackgroundColor).to.have.been.calledWith({
+				color: Colors.DEFAULT,
+			});
+		});
+
+		it('sets zero count', () => {
+			renderWithState({
+				auth: { twitch: { token: '', user: { login: '' } } },
+				streams: {
+					data: [],
+					lastReceived: {
+						twitch: Date.now(),
+					},
+				},
+				isOnline: true,
+			});
+
+			expect(browser.browserAction.setBadgeText).to.have.been.calledWith({ text: '0' });
+			expect(browser.browserAction.setBadgeBackgroundColor).to.have.been.calledWith({
+				color: Colors.DEFAULT,
+			});
 		});
 	});
 });
